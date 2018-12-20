a1d9b60a2972da7ef15157e7142e52520b0392b3
sadturn <loliwinsmeow@gmail.com>
2018-12-20 18:40:08 +0200

    add pages&reviews controllers, review model

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+end
